22cfd88410d018891061e8eebaf55fc373e6dc40
kawa <kawa@kawa-P55A-UD3>
2013-11-26 19:45:36 +0100

    Several changes on layoutCPN Full implement of navigation CPN

---
+++ components/interface/navigation.js
@@ -0,0 +1,65 @@
+/* Navigation component.
+ * Top navigation single item.                                                  */
+
+
+function NavigationCPN(ctn, source) {
+    Toolkit.checkClassOf(source, jQuery);
+    
+    var c = new Component(ctn, "build/navigation.xml");
+    
+    c.registerMethod(NavigationCPN.prototype.init, "init", false);
+    c.registerMethod(NavigationCPN.prototype.wrapperLeft, "wrapperLeft", false);
+    c.registerMethod(NavigationCPN.prototype.wrapperWidth, "wrapperWidth", false);
+    
+    c.register("source", source);
+    
+    return c;
+}
+NavigationCPN.prototype.init = function() {
+    var ctx = this;
+    
+    // Labelizing
+    var label = $(this.source).children("i.label").text();
+    this.qs("linkBack", "p").addClass("bundle-" + label);
+    this.qs("linkFront", "p").addClass("bundle-" + label);
+    this.qs("innerDescription").addClass("bundle-" + $(this.source).children("i.description").text());
+    $(this.source).children("s.sublink").each(function() {
+        var buff =  '<a href="' + $(this).children("i.to").text() + '">' +
+                        '<li>' +
+                            '<span class="bundle-' + $(this).children("i.label").text() + '"></span>' +
+                            '<span class="bundle-' + $(this).children("i.description").text() + '"></span>' +
+                        '</li>' +
+                    '</a>';
+        ctx.qs("innerList").append(buff);
+    });
+    Register.getByName("translator").qc("build", true, this.container);
+    
+    // Colorizing
+    var color = $(this.source).children("i.color").text();
+    this.qs("link").addClass("force-" + color);
+    this.qs("linkFront").addClass("force-" + color);
+    this.qs("linkBack").addClass("force-" + color);
+    this.qs("wrapper").addClass("force-" + color);
+    this.getSelector("innerListElements", true).getNodes().addClass("force-" + color + " spe-colorHover");
+    Register.getByName("palette").qc("refresh", this);
+    
+    // Positionning wrapper
+    this.qs("wrapper").css({
+        top: this.qs("$SELF").outerHeight(true) + "px",
+        left: this.qc("wrapperLeft"),
+        width: this.qc("wrapperWidth")
+    });
+};
+NavigationCPN.prototype.wrapperLeft = function() {
+    var ctx = this;
+    var left = 0;
+    this.qs("$SELF").parent().children().each(function(i) {
+        if (i < $(ctx.container).index()) {
+            left += parseFloat(window.getComputedStyle(this).width);
+        }
+    });
+    return -left + "px";
+};
+NavigationCPN.prototype.wrapperWidth = function() {
+    return this.qs("$SELF").parent().width() + "px";
+};
--- components/layout/layout.js
+++ components/layout/layout.js
@@ -7,6 +7,13 @@ function LayoutCPN(ctn) {
     
     c.registerMethod(LayoutCPN.prototype.init, "init", false);
     c.registerMethod(LayoutCPN.prototype.defineBodyHeight, "defineBodyHeight", false);
+    c.registerMethod(LayoutCPN.prototype.buildNavigation, "buildNavigation", false);
+    c.registerMethod(LayoutCPN.prototype.startNavigation, "startNavigation", false);
+    
+    var cb = [
+        {method: "buildNavigation"}
+    ];
+    c.registerSource("navigation", "data/content/navigation.xml", "Minimal", cb);
     
     c.saveInterface(LoadableITF);
     c.qc("addTarget@Loadable", "background");
@@ -16,27 +23,46 @@ function LayoutCPN(ctn) {
     c.qc("addForMixedOverflow@Enhanceable", "background");
     
     c.saveInterface(ColumnableITF);
-    c.qc("addStatic@Columnable", "navigationInner");
+    c.qc("addStatic@Columnable", "bodyNavigation");
     
     return c;
 }
+
 LayoutCPN.prototype.init = function() {
     var t = new TranslatorPI(this.qs("virtuals", "div.translator"));
-    
-    Register.identify(t, "translator");
     t.start();
     t.qc("load", "fr", "bundle_fr");
     t.qc("load", "en", "bundle_en");
-    
-    t.qc("addAlways", "testA", this, "bodyCore", "p:first()");
-    t.qc("addAlways", "testB", this, "bodyCore", "p:last()");
-    
     t.qc("switch", "fr");
+    
+    var p = new PalettePI(this.qs("virtuals", "div.palette"));
+    p.start();
+    p.qc("switch", "blue", true);
+    
+    Register.identify(t, "translator");
+    Register.identify(p, "palette");
+};
+LayoutCPN.prototype.buildNavigation = function() {
+    var ctx = this;
+    $(this.getSourceData("navigation", "s.link")).each(function(i) {
+        var n = this;
+        setTimeout(function() {
+            new NavigationCPN(ctx.qs("bodyNavigationLinks").eq(i), $(n)).start();
+        }, 150 * i);
+    });
+};
+LayoutCPN.prototype.startNavigation = function() {
+    var ctx = this;
+    this.qs("bodyNavigationLinks").each(function(i) {
+        var n = this;
+        setTimeout(function() {
+            Register.getFrom(n).go("Close");
+        }, 150 * i);
+    });
 };
 LayoutCPN.prototype.defineBodyHeight = function() {
     var h = this.qs("$BODY").height() - 
             this.qs("header").outerHeight(true) - 
-            this.qs("navigation").outerHeight(true) - 
             this.qs("footer").outerHeight(true);
     this.register("body_height", h < 640 ? "640px" : h + "px", true);
 };
